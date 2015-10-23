beed95962116d5ed76f5ce71964106dcd9096fdb
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2015-10-22 21:19:36 -0500

    Sp3ak3r 3l1t3 x00x0x0x00x0x0x
    camgirl.exe

---
+++ Speaker.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.applet.*;
+
+class Speaker
+{
+   public static void drawSpeaker(Graphics g, int sx, int sy)
+   {
+      g.setColor(new Color(12,12,12));
+      g.fillRect(250, 250, sx, sy);
+      
+   }
+}
