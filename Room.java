6d53a4415072ab7d1818f399d4906e1898c7e6f0
LukasMMarinovic <lukas@agramer.com>
2015-10-22 21:36:59 -0500

    HD Corn
    yumyum

--- Room.java
+++ Room.java
@@ -14,13 +14,19 @@ class Room
       g.fillRect(0,0,800,600);
       
       //generate random bluish color...
-      red = ((int) (Math.random()*256));
-      green  = ((int) (Math.random()*256));
+      red = 89;
+      green  = ((int) ((Math.random()*45)+109));
       blue = 168;         
-      g.setColor(new Color(red,green,blue));
-      
+      g.setColor(new Color(red,green,blue));     
       //draw stage
       g.fillRect(150,150,500,300);
-       
+      
+      //draw edges
+      g.setColor(new Color(0,0,0));
+      g.drawLine(0,0,150,150);
+      g.drawLine(0,600,150,450);
+      g.drawLine(650,150,800,0);
+      g.drawLine(650,450,800,600);
+      
    }
 }  
