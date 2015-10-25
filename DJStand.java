3ca559b6c875bd8f63414fd3751f8bef14470800
handroishonda <handro2010@live.com>
2015-10-25 17:58:42 -0500

    shapeshifting tick
    dj stand actually has stuff this time .exe

--- DJStand.java
+++ DJStand.java
@@ -5,12 +5,40 @@ class DJStand
    {
       public static void drawDJStand(Graphics g)
       {
-         g.setColor(new Color(0,0,0)); //black cock
-         g.fillRect(150,400,500,50);   //stage
-         g.fillRect(280,330,240,70);   //table
-         g.fillRect(295,315,75,15);    //disk one 
-         g.fillRect(430,315,75,15);    //disk two
-         g.fillRect(150,400,500,50);
+         g.setColor(new Color(0,0,0));     //black cock
+         g.fillRect(150,400,500,50);       //stage
+         g.setColor(new Color(40,40,40));  //gray cock
+         g.fillRect(280,330,240,70);       //table
+         g.setColor(new Color(20,20,20));
+         g.fillRect(295,315,75,15);        //disk one 
+         
+         int ranX1 = ((int)(Math.random() * 50) + 299);
+         int ranX2 = ((int)(Math.random() * 50) + 310);
+         
+         g.setColor(new Color(255,255,255));
+         g.drawLine(ranX1,318,ranX2,318);
+         g.drawLine(ranX1,323,ranX2,323); 
+         g.drawLine(ranX1,328,ranX2,328);
+         
+         g.setColor(new Color(20,20,20));
+         g.fillRect(430,315,75,15);        //disk two
+        
+         ranX1 = ((int)(Math.random() * 50) + 434);
+         ranX2 = ((int)(Math.random() * 50) + 435);
+         
+         g.setColor(new Color(255,255,255));
+         g.drawLine(ranX1,318,ranX2,318);
+         g.drawLine(ranX1,323,ranX2,323); 
+         g.drawLine(ranX1,328,ranX2,328);
+
+         
+         
+
+             
+        
+         
          
       }
+     
+
    }
