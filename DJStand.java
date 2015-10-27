da44ed057a1dffc4d8817a7435176f5156bd5d27
handroishonda <handro2010@live.com>
2015-10-27 07:50:14 -0500

    suck my big fat code
    sorry vijay

--- DJStand.java
+++ DJStand.java
@@ -34,35 +34,53 @@ class DJStand
       public static void drawEqualizer(Graphics g)
       {
          g.setColor(new Color(0,0,0));
-         g.fillRect(425,335,80,63);
+         g.fillRect(425,333,80,65);
          for(int t=0; t<=60; t+=12)
          {
          
             int yDifference = 0;
-            int barHeight = (int)(Math.random() * 4);
+            int barHeight = (int)(Math.random() * 5);
             int height = 0;
             switch(barHeight)
             {
                
                case 0: height = 12;
                        yDifference = 48;
+                       g.setColor(new Color(2,247,7));
                        break;
                case 1: height = 24;
                        yDifference = 36;
+                       g.setColor(new Color(170,247,2));
                        break;
                case 2: height = 36;
                        yDifference = 24;
+                       g.setColor(new Color(235,247,2));
                        break;
                case 3: height = 48;
                        yDifference = 12;
+                       g.setColor(new Color(247,125,2));
                        break;
                case 4: height = 60;
                        yDifference = 0;
+                       g.setColor(new Color(247,43,2));
                        break;
+                       
             }
-         g.setColor(new Color(255,255,255));
+         
          g.fillRect((430 + t),(335 + yDifference),10,height);
          }
+         g.setColor(new Color(0,0,0));
+         g.fillRect(425,340,80,2);
+         g.fillRect(425,346,80,2);
+         g.fillRect(425,352,80,2);
+         g.fillRect(425,358,80,2);
+         g.fillRect(425,364,80,2);
+         g.fillRect(425,370,80,2);
+         g.fillRect(425,376,80,2);
+         g.fillRect(425,382,80,2);
+         g.fillRect(425,388,80,2);
+         g.fillRect(425,394,80,2);
+         g.fillRect(425,400,80,2);
          
         }
       
