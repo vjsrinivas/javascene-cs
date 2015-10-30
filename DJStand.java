02fb7466fc012c6c7936e118fcd77c168303c41f
handroishonda <handro2010@live.com>
2015-10-29 19:56:44 -0500

    Indiaaaaaaa
    added the indian flare this program needed

--- DJStand.java
+++ DJStand.java
@@ -34,7 +34,7 @@ class DJStand
       public static void drawEqualizer(Graphics g)
       {
          g.setColor(new Color(0,0,0));
-         g.fillRect(425,333,80,65);
+         g.fillRect(425,333,80,65);  //background rectangle
          for(int t=0; t<=60; t+=12)
          {
          
@@ -46,30 +46,30 @@ class DJStand
                
                case 0: height = 12;
                        yDifference = 48;
-                       g.setColor(new Color(2,247,7));
+                       g.setColor(new Color(2,247,7));   //green
                        break;
                case 1: height = 24;
                        yDifference = 36;
-                       g.setColor(new Color(170,247,2));
+                       g.setColor(new Color(170,247,2)); //little less green
                        break;
                case 2: height = 36;
                        yDifference = 24;
-                       g.setColor(new Color(235,247,2));
+                       g.setColor(new Color(235,247,2)); //little more yellow
                        break;
                case 3: height = 48;
                        yDifference = 12;
-                       g.setColor(new Color(247,125,2));
+                       g.setColor(new Color(247,125,2)); //little more orange
                        break;
                case 4: height = 60;
                        yDifference = 0;
-                       g.setColor(new Color(247,43,2));
+                       g.setColor(new Color(247,43,2));  //red
                        break;
                        
             }
          
-         g.fillRect((430 + t),(335 + yDifference),10,height);
+         g.fillRect((430 + t),(335 + yDifference),10,height);  //bars
          }
-         g.setColor(new Color(0,0,0));
+         g.setColor(new Color(0,0,0));   //rectangles dividing bars into rectangles
          g.fillRect(425,340,80,2);
          g.fillRect(425,346,80,2);
          g.fillRect(425,352,80,2);
@@ -81,9 +81,32 @@ class DJStand
          g.fillRect(425,388,80,2);
          g.fillRect(425,394,80,2);
          g.fillRect(425,400,80,2);
-         
+        
+        
         }
       
-     
+        public static void drawIndianStuff(Graphics g)
+        {
+        
+        g.setColor(new Color(0,0,0));    //rectangle encasing text
+        g.fillRect(288,333,120,65);
+        
+        g.setColor(new Color(255,153,51));    //saffron red
+        g.fillRect(292,335,112,20);
+        g.setColor(new Color(255,255,255));   //white
+        g.fillRect(292,355,112,20);
+        g.setColor(new Color(19,136,8));      //india green
+        g.fillRect(292,375,112,20);
+        
+        g.setColor(new Color(255,50,50));  //Indian dot
+        g.fillRect(380,225,3,4);
+        
+        g.setColor(new Color(0,0,160));    //India blue
+        int fontSize = 20;
+        g.setFont(new Font("Comic Sans",Font.PLAIN,fontSize));
+        g.drawString("DJ VJ",320,373);
+        
+            
+        }
  
    }
